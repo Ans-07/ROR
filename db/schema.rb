bff417525d4748e2d53d810a58cba45ea716df53
Ans-07 <ans.ishfaq@codingcops.com>
2025-02-12 17:20:47 +0500

    Portfolio With Migrated Db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_12_093539) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_12_115357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
