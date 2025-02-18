f5d4be11634f08f6767947c252078e5175fbe834
Ans-07 <ans.ishfaq@codingcops.com>
2025-02-18 13:17:55 +0500

    Working With Enums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_17_135323) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_17_164957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_17_135323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "state", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
